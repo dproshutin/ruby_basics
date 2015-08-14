1b51efc65cf975c3c58d197c4986603dae8d44cd
Dmitriy Proshutin <Dmitry.Proshutin@gmail.com>
2015-08-14 20:03:21 +0600

    Add a newline

--- display_name.rb
+++ display_name.rb
@@ -2,9 +2,10 @@
   lname = 'Proshutin'
 
   # String concatenation
-  puts "Result of string concatenation"
+  puts "Result of string concatenation:"
   puts fname + " " + lname
 
   # String interpolation
-  puts "Result of string interpolation"
+  puts "Result of string interpolation:"
   puts "#{fname} #{lname}"
+  # new line added
